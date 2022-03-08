29092eceda9842fc4b038d16f79ffc47b2fda511
vagrant <vagrant@homestead>
2022-03-08 15:47:57 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
     <title>@yield('title','Weibo App y') - 由 Luis 制作的weibo</title>
 </head>
 <body>
-    <h2>这里是主内容部分哦</h2></a>@yield('content')
-
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    </a>@yield('content')
+  </div>
 </body>
 </html>
