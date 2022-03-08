c2f09b9e9dca3ef7fadf03c34f37f3c6f238d2fe
vagrant <vagrant@homestead>
2022-03-08 14:10:31 +0000

    模版切分

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>关于</title>
-</head>
-<body>
-    <h1>关于页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title','关于 Y')
+@section('content')
+    <h1>关于页33</h1>
+@endsection
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>帮助</title>
-</head>
-<body>
-    <h1>帮助页</h1>
-</body>
-</html>
+@extends('layouts.default')
+
+
+@section('content')
+<h1>帮助页2</h1>
+@section('title','帮助 y')
+@endsection
