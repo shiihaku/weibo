61a095347912b5f02a3bd049bb150a4792f37e14
vagrant <vagrant@homestead>
2022-03-13 07:29:07 +0000

    创建登陆会话

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,7 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class SessionsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', 'Welcome back!');
+            return redirect()-> route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', 'Sorry,email or password is incorrect!');
+            return redirect()->back()->withInput();
+        }
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+// 登陆部分路由
+Route::get('login', 'SessionsController@create')->name('login');
+Route::get('lgoin', 'SessionsController@store')->name('login');
+Route::delete('login', 'SessionsController@destory')->name('logout');
